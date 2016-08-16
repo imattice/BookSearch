475f9a55efd51c90d80d7bcd3bf46e3062088224
Ike <ike.mattice@tiger-sheep.com>
2016-08-16 16:44:41 -0700

    remove fake data and use google books api.

--- BookList.js
+++ BookList.js
@@ -6,24 +6,83 @@ import {
   View,
   Image,
   Text,
+  ListView,
+  TouchableHighlight,
+  ActivityIndicator,
 } from 'react-native';
 
-var FAKE_BOOK_DATA = [
-  {volumeInfo: {title: 'The Catcher in the Rye', authors: "J. D. Salinger", imageLinks: {thumbnail: 'http://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}}}
-]
+var REQUEST_URL = 'https://www.googleapis.com/books/v1/volumes?q=subject:fiction'
 
 class BookList extends Component {
-  render() {
-    var book = FAKE_BOOK_DATA[0]
-    return(
-      <View style={styles.container}>
-      <Image source={{uri: book.volumeInfo.imageLinks.thumbnail}}
-          style={styles.thumbnail} />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{book.volumeInfo.title}</Text>
-            <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+
+  constructor(props){
+    super(props)
+    this.state = {
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(responseData.items),
+        isLoading: false,
+      })
+    })
+    .done()
+  }
+
+  renderBook(book) {
+    return (
+      <TouchableHighlight>
+        <View>
+          <View style={styles.container}>
+            <Image
+              source={{uri: book.volumeInfo.imageLinks.thumbnail}}
+              style={styles.thumbnail} />
+            <View style={styles.rightContainer}>
+              <Text style={styles.title}>{book.volumeInfo.title}</Text>
+              <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+            </View>
           </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator
+          size='large' />
+        <Text>
+          Loading books...
+        </Text>
       </View>
+    )
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return this.renderLoadingView()
+    }
+
+    return(
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderBook.bind(this)}
+        style={styles.listView}
+      />
     )
   }
 }
@@ -53,6 +112,19 @@ var styles = StyleSheet.create({
   author: {
     color: '#656565'
   },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd',
+  },
+  listView: {
+    marginTop: 55,
+    backgroundColor: '#F5FCFF',
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
 
 module.exports = BookList
